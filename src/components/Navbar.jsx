5f5dbade2a257d5458bdb82ece188b519d70abee
Sanyam <sharmasanyam69>
2024-09-08 00:49:33 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-28" src="./images/Logo.png" alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/sanyamsharma98/" target="_blank">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/DARKHACK4949" target="_blank">
+          <FaGithub />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1f_1_SbjJbwq8otGpgmEk0uGh4uPNpP20/view?usp=sharing"
+          target="_blank"
+          className="flex items-center"
+        >
+          <span className="rounded bg-neutral-300 px-2 py-1 text-sm font-medium text-neutral-900">
+            Resume
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,81 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Passionate about blending design and technology, I craft intuitive and visually stunning digital experiences. With expertise in HTML, CSS, JavaScript, and React.js, I transform complex concepts into responsive, high-performance interfaces. Leveraging Figma and Adobe XD, I guide projects from initial wireframes to polished, user-centric solutions. My approach focuses on understanding user needs and delivering innovative designs that captivate and engage, ensuring every product is both beautiful and functional.`;
+
+export const ABOUT_TEXT = `I am a dedicated Product Designer and Front-End Developer with a strong passion for creating intuitive and visually compelling digital experiences. With a solid foundation in front-end technologies like HTML, CSS, JavaScript, and React.js, I specialize in translating complex design concepts into responsive and accessible interfaces that engage users and drive results.
+
+My expertise extends to product design tools like Figma and Adobe XD, allowing me to craft seamless user experiences from initial wireframes to final implementation. I thrive at the intersection of design and development, ensuring that every product I work on is not only visually appealing but also highly functional and optimized for performance.
+
+My approach is rooted in a deep understanding of user needs, attention to detail, and a commitment to delivering high-quality solutions that exceed expectations. Whether collaborating with cross-functional teams or working independently, I am driven by the challenge of bringing innovative ideas to life and continuously improving the user journey.`;
+
+export const EXPERIENCES = [
+  {
+    year: "September - 2024",
+    role: "Product Design virtual experience",
+    company: "Accenture North America",
+    description: `Completed a simulation focused on how the Product Design team can transform a user's experience. Added a new feature and iterated on an existing product screen. Communicated the decisions made for the feature design`,
+    technologies: ["Figma", "UI Design", "UX Design", "Rationale", "Product Design"],
+    certificateLink: "https://drive.google.com/file/d/1dKtVNwUk9jlxa2HkuadNpxaJ3462ntXN/view?usp=sharing"  
+  },
+  {
+    year: "August - 2024",
+    role: "Software Engineering Virtual Experience",
+    company: "J.P. Morgan",
+    description: `Set up a local dev environment by downloading the necessary files, tools and dependencies. Fixed broken files in the repository to make web application output correctly. Used JPMorgan Chase’s open source library called Perspective to generate a live graph that displays a data feed in a clear and visually appealing way for traders to monitor.`,
+    technologies: ["React", "Python", "TypeScript", "Technical Communication",],
+    certificateLink: "https://drive.google.com/file/d/1edcmQNqDlJq5zLodStWs-0NxzTbU62jb/view?usp=sharing" 
+  },
+  {
+    year: "September - 2023",
+    role: "Full Stack Developer Intern",
+    company: "Bharat Intern",
+    description: `Contributed to two key projects: Project Management Tool and Content Management Tool. Improved functionalities and user experience by implementing essential features with HTML, CSS, and JavaScript, enhancing workflow efficiency in both systems.`,
+    technologies: ["HTML", "CSS", "JavaScript",],
+    certificateLink: "https://drive.google.com/file/d/19-puy0tCFGTQcBagskaxjrgRrIUzr6YI/view?usp=sharing"  
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "BrainWave Website",
+    image: project1,
+    description:
+      "BrainWave is a sleek UI/UX website developed using React.js and Tailwind CSS, featuring seamless animations and a modern design that sets a new standard for future web applications.",
+    technologies: ["React", "Tailwind CSS", "Vite"],
+    link: "https://master--brainwavehub.netlify.app/" 
+  },
+  {
+    title: "HealthFull",
+    image: project2,
+    description:
+      "HealthFull is a modern web app promoting millets as a healthy, sustainable choice. Built with React, Tailwind CSS, and a strong backend, it offers a smooth, mobile-first experience to inspire healthier living.",
+    technologies: ["React", "Express.js", "Mongo db", "Node.js"],
+    link: "https://diet-application.netlify.app/" 
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS", "Vite"],
+    link: "https://example.com/portfolio"  
+  },
+  {
+    title: "Project Management Tool",
+    image: project4,
+    description:
+      "A web-based application that simplifies task management and team collaboration. Built with HTML, CSS, and JavaScript, it enables users to create, assign, and track tasks efficiently, with real-time progress updates and customizable workflows.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    link: "https://darkhack4949.github.io/Project-management-tool-/"  
+  },
+];
+
+
+export const CONTACT = {
+  address: "Palampur, Himachal Pradesh, IN 176081 ",
+  phoneNo: "+91 98162 15258 ",
+  email: "sanyamsharma4978@gmail.com",
+};
