abbe87a6a08b212730b2096c29c757b8be66d5e6
Sanyam <sharmasanyam69>
2025-03-17 23:05:44 +0530

    updated

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -7,6 +7,11 @@ const designs = [
     link: "https://www.figma.com/proto/InWC3pvUaDsLnAycsAdLKA/Local-Link-(Case-Study)?page-id=0%3A1&node-id=4-2&viewport=49%2C404%2C0.11&t=f4Oc19xMQcmzP2qZ-1&scaling=scale-down&content-scaling=fixed",
   },
   {
+    image: "./images/ui-11.png", 
+    title: "Wallsy (Wallpaper App)",
+    link: "https://www.figma.com/proto/07w4NsgZlW6vICp6XYrKn7/Wallsy-(Wallpaper-Application)?page-id=0%3A1&node-id=2-2&p=f&viewport=244%2C363%2C0.08&t=5W0WgdFGHRPHsbpN-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2%3A2",
+  },
+  {
     image: "./images/ui-2.png", 
     title: "Website Landing Page",
     link: "https://www.figma.com/proto/tEgIdHoWnFHDHkhJqWWTVn/Responsive-Landing-Page-Design-%7C-Website-Home-Page-Design-%7C-Agency-Website-UI-Design?page-id=0%3A1&node-id=5-573&node-type=canvas&viewport=394%2C1002%2C0.38&t=WjphnViuePXoCxUZ-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=5%3A573",
